a308c351613ffb58e667d9caff8a34c8bd518aff
Naitik Srivastava <naitiksrivastava123fzd@gmail.com>
2025-08-07 11:31:36 +0530

    Time: 99 ms (81.09%), Space: 8.6 MB (59.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,18 +1,24 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* result = (int*)malloc(2 * sizeof(int)); // Allocate memory for 2 indices
-    *returnSize = 2;
-
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] + nums[j] == target) {
-                result[0] = i;
-                result[1] = j;
-                return result;
-            }
-        }
+ int* result= (int*)malloc(sizeof(int)*2);
+ *returnSize = 2;
+ for(int i=0;i<numsSize;i++)
+ {
+  for(int j=i+1;j<numsSize;j++)
+  {
+  if(nums[i]+nums[j]==target)
+    {
+        // printf("%d\n",target);
+    //   printf("%d and %d\n",i,j);
+      result[0]=i;
+      result[1]=j;
+      return result;
+    
+     break;
     }
-
-    // Fallback (though we assume there's always one solution)
+  }
+ }
+     free(result);
     *returnSize = 0;
     return NULL;
+
 }
