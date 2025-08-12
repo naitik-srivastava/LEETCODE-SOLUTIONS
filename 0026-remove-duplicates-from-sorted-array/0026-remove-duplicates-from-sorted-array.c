455eee18fec6c9e71aacf5146d63f3fc1a2acc84
Naitik Srivastava <naitiksrivastava123fzd@gmail.com>
2025-08-12 13:14:08 -0400

    Time: 0 ms (100%), Space: 12.4 MB (12.09%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -1,54 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;
 
-void removeElement(int *nums, int *size, int pos) {
-    for (int i = pos; i < *size - 1; i++) {
-        nums[i] = nums[i + 1];
-    }
-    (*size)--; // reduce logical size
-}
-
-int removeDuplicates(int *nums, int numsSize) {
-    if (numsSize <= 1) return numsSize;
-
-    int *recDupesIndex = (int *)malloc(sizeof(int) * numsSize);
-    if (!recDupesIndex) return numsSize; // guard
-
-    int countDup = 0;
-
-    // Record duplicate indices once per value (skip non-first occurrences of same value)
-    for (int i = 0; i < numsSize - 1; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) continue; // skip i if it's not the first of its value
-
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] == nums[j]) {
-                recDupesIndex[countDup++] = j;
-            } else {
-                break; // sorted array: once mismatch occurs, no more duplicates for this i
-            }
+    int k = 1; // index for unique elements
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[k - 1]) {
+            nums[k] = nums[i];
+            k++;
         }
     }
-
-    // Remove from back to front so earlier removals don't shift later indices
-    for (int k = countDup - 1; k >= 0; k--) {
-        removeElement(nums, &numsSize, recDupesIndex[k]);
-    }
-
-    free(recDupesIndex);
-    return numsSize; // new length (number of unique elements)
+    return k; // number of unique elements
 }
-
-// int main() {
-//     int nums[] = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-//     int numsSize = sizeof(nums) / sizeof(nums[0]);
-
-//     int newLength = removeDuplicates(nums, numsSize);
-
-//     printf("New length: %d\n", newLength);
-//     printf("Modified array: ");
-//     for (int i = 0; i < newLength; i++) {
-//         printf("%d ", nums[i]);
-//     }
-//     printf("\n");
-//     return 0;
-// }
