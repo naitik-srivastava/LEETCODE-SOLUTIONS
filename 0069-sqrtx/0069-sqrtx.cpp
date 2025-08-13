5812c2cca6606fa16555bf8f2646fd7798f7e35f
Naitik Srivastava <naitiksrivastava123fzd@gmail.com>
2025-08-13 16:00:09 -0400

    Time: 0 ms (100%), Space: 8.6 MB (14.56%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+            // int num = 8;
+    int root = sqrt(x);  // calculates square root
+    // cout << "Square root of " << num << " is " << root << endl;
+    return root;
+
+    }
+};
